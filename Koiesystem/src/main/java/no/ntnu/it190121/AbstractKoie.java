e0d4c326cff4338afcf7b99668502aebbb819647
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-09-10 15:26:30 +0200

    initial abstractkoie superclass

--- Koiesystem/src/main/java/no/ntnu/it190121/AbstractKoie.java
+++ Koiesystem/src/main/java/no/ntnu/it190121/AbstractKoie.java
@@ -1,5 +1,11 @@
 package no.ntnu.it190121;
 
-public abstract AbstractKoie {
+public abstract class AbstractKoie {
+
+    public abstract String getName();
+    public abstract String getAddress();
+    public abstract int getNumBeds();
+    public abstract int getNumSeats();
+    protected abstract String getId();
 
 }
