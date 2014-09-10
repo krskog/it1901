725915a6b1559493e6bc3cfa1cd1e2d27323de7d
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-09-10 12:20:52 +0200

    finished setup

---
+++ Koiesystem/src/main/java/no/ntnu/it1901-21/App.java
@@ -0,0 +1,13 @@
+package no.ntnu.it1901-21;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
