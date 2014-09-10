3fa76f07f2ff016155545813ca1d8dbf89004e71
Sklirg <hlsolbjorg@gmail.com>
2014-09-10 17:34:53 +0200

    added some more abstract methods

--- Koiesystem/src/main/java/no/ntnu/it190121/AbstractKoie.java
+++ Koiesystem/src/main/java/no/ntnu/it190121/AbstractKoie.java
@@ -1,11 +1,22 @@
 package no.ntnu.it190121;
 
+import java.util.Collections;
+
 public abstract class AbstractKoie {
 
+    protected abstract String getId();
     public abstract String getName();
     public abstract String getAddress();
     public abstract int getNumBeds();
     public abstract int getNumSeats();
-    protected abstract String getId();
-
+    public abstract int getBuildYear();
+    /*
+    public abstract String getTerrain();
+    public abstract boolean hasBike();
+    public abstract boolean hasMountains();
+    public abstract boolean hasHuntFish();
+    public abstract boolean hasGuitar();
+    public abstract boolean hasWaffles();
+    public abstract Collections getSpecialities();
+    */
 }
