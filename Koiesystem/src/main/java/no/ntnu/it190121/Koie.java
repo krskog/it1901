9169773ca7753ff53011ce666b3445b277d98883
Ziphone <simonslyngstad@Simons-MacBook-Pro.local>
2014-09-14 16:11:36 +0200

    created two new classes - koie and lokasjon

---
+++ Koiesystem/src/main/java/no/ntnu/it190121/Koie.java
@@ -0,0 +1,12 @@
+package no.ntnu.it190121;
+
+public class Koie {
+	public final String navn;
+	public final int soveplasser;
+	public final Lokasjon lokasjon;
+	public Koie(String name, int soveplasser, Lokasjon lokasjon){
+		this.navn = name;
+		this.soveplasser = soveplasser;
+		this.lokasjon = lokasjon;
+	}
+}
