62d9324e88550bd3b8c55cb80523cbea25aa933d
Sklirg <hlsolbjorg@gmail.com>
2014-10-08 16:25:56 +0200

    removed some comments 'n stuff

--- koieadmin/koie/management/commands/import_new_koies.py
+++ koieadmin/koie/management/commands/import_new_koies.py
@@ -4,17 +4,13 @@ from koie.models import Koie
 
 import json
 
-# Import
 f = 'koiedump.json'
 json_data = ""
 
 with open(f, 'r') as infile:
     raw_data = infile.read()
-  #  print(raw_data)
     json_data = json.loads(raw_data)
- #   print(json_data)
 
-#print(json_data["1"])
 class Command(BaseCommand):
     args = "test1"
     help = "test2"
@@ -29,9 +25,6 @@ class Command(BaseCommand):
             koie = json_data[koie]
             
             # Check if koie already exists
-
-#            print(koie['name'])
-#            print(Koie.objects.filter(name=koie['name']), Koie.objects.filter(name=koie['name']).count())
             if Koie.objects.filter(name=koie['name']).count() > 0:
                 existing += 1
                 continue
@@ -44,17 +37,3 @@ class Command(BaseCommand):
             k.save()
             new += 1
         self.stdout.write('%s koies looked at. %s were already in the system, so %s new koies were added.' % (koies, existing, new))
-
-"""
-x = 0
-for koie in json_data:
-    k = Koie()
-    k.name = koie['name']
-    k.address = koie['address']
-    k.location = koie['location']
-    k.zip_code = koie['zip_code']
-    k.num_beds = koie['num_beds']
-    k.save()
-    x += 1
-print('Added %s koies to koiesys' % x)
-"""
