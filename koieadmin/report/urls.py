914a4180611c49822dcfbf0c5f380da32b6fc785
Sklirg <hlsolbjorg@gmail.com>
2014-10-21 08:56:51 +0200

    Removed unused app directories

--- koieadmin/report/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from report import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<report_id>\d+)/$', views.detail, name='detail'),
-)
