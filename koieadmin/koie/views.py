366573d9ed99cc63c2213508547edc3cabe48d93
Ziphone <simonslyngstad@dhcp-027186.wlan.ntnu.no>
2014-10-27 13:51:01 +0100

    Endring i models, read_date

--- koieadmin/koie/views.py
+++ koieadmin/koie/views.py
@@ -174,7 +174,7 @@ def get_future_reservations(koie=None, num=10):
 ### Latest reports
 
 def get_latest_reports():
-    return Report.objects.filter(read=False)
+    return Report.objects.filter(read_date=None)
 
 def get_specific_report(id):
     return Report.objects.filter(id = id)
