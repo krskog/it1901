56f8bff37f0b52d3789c658462f5bcab19823741
sklirg <hlsolbjorg@gmail.com>
2014-11-04 17:54:09 +0100

    Initial date widget

--- koieadmin/koie/forms.py
+++ koieadmin/koie/forms.py
@@ -2,12 +2,17 @@ from django import forms
 from django.forms import ModelForm
 from koie.models import Reservation, Report, Damage
 
+from django.forms.extras.widgets import SelectDateWidget
+from datetime import date
+
 class ReservationForm(ModelForm):
     email = forms.EmailField(label='Your email', max_length=100)
+    rent_date = forms.DateField(label="Rent date", widget=SelectDateWidget, initial=date.today())
 
     class Meta:
         model = Reservation
         exclude = ('ordered_by', 'ordered_date',)
+
 
 class ReportForm(ModelForm):
     damages = forms.CharField(label='Her kan eventuelle skader fylles inn. I folgende format: skade1 -- skade2 -- skade3...', required=False)
